c047f2f33cf682f1b15f885e4377f57482a7f7b5
Anna Luiza <luizafistarol@gmail.com>
2023-02-27 22:06:46 -0300

    feat(strings): edit informações

--- php_1/strings.php
+++ php_1/strings.php
@@ -8,6 +8,6 @@ echo "Olá mundo,! Eu sou a $nome, tenho $idade anos e sou uma dev fullstack";
 
 echo "Para quebrar linhas precisa digitar => \n <= dessa forma";
 echo "Ou => \r\n <= dessa forma";
-echo "Ou =>" . PHP_EOL ."<= dessa forma";
+echo "Mas a MELHOR FORMA DE QUEBRAR LINHAS é =>" . PHP_EOL ."<= dessa forma";
 
 echo "Para criar um tab precisa digitar => \t <= dessa forma";

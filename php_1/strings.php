51302ff456ec2f5e715c7e58e9247f36026737c5
Anna Luiza <luizafistarol@gmail.com>
2023-02-27 22:04:35 -0300

    feat: add informações sobre strinigs em php

---
+++ php_1/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = 'Anna Luiza';
+$idade = 23;
+
+echo 'Olá mundo,! Eu sou a ' . $nome . ', tenho ' . $idade . ' anos e sou uma dev fullstack';
+echo "Olá mundo,! Eu sou a $nome, tenho $idade anos e sou uma dev fullstack";
+
+echo "Para quebrar linhas precisa digitar => \n <= dessa forma";
+echo "Ou => \r\n <= dessa forma";
+echo "Ou =>" . PHP_EOL ."<= dessa forma";
+
+echo "Para criar um tab precisa digitar => \t <= dessa forma";

260906482596651f701d4f3978374e1e473c1e68
Anna Luiza <luizafistarol@gmail.com>
2023-02-28 09:33:41 -0300

    feat: update loop for

--- php_1/repeticaoFor.php
+++ php_1/repeticaoFor.php
@@ -8,3 +8,26 @@
 for ($contador = 1; $contador <= 15; $contador++) {
     echo "#$contador" . PHP_EOL;
 };
+
+//PARA PULAR UM NÚMERO = FORMA ERRADA, PORQUE QUANDO TEMOS CASOS EXCEPCIONAIS DEVEMOS RELATAR PRIMEIRO
+for ($contador = 1; $contador <= 15; $contador++) {
+    if ($contador != 13) {
+        echo "#$contador" . PHP_EOL;        
+    };
+};
+
+//PARA PULAR UM NÚMERO = FORMA CORRETA => PULANDO A ITERAÇÃO
+for ($contador = 1; $contador <= 15; $contador++) {
+    if ($contador == 13) {
+       continue;
+    };
+    echo "#$contador" . PHP_EOL;     
+};
+
+//PARA SAIR DO LOOP =
+for ($contador = 1; $contador <= 15; $contador++) {
+    if ($contador == 13) {
+       break;
+    };
+    echo "#$contador" . PHP_EOL;     
+};

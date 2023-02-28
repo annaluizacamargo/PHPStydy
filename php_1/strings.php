555f2889a9a66e50c7e3aac014850c3905424d54
Anna Luiza <luizafistarol@gmail.com>
2023-02-27 22:10:09 -0300

    feat(strings): edit informações

--- php_1/strings.php
+++ php_1/strings.php
@@ -4,10 +4,22 @@ $nome = 'Anna Luiza';
 $idade = 23;
 
 echo 'Olá mundo,! Eu sou a ' . $nome . ', tenho ' . $idade . ' anos e sou uma dev fullstack';
+echo PHP_EOL;
+
 echo "Olá mundo,! Eu sou a $nome, tenho $idade anos e sou uma dev fullstack";
+echo PHP_EOL;
+
+echo "Eu tenho \"$idade\" anos";
+echo PHP_EOL;
 
 echo "Para quebrar linhas precisa digitar => \n <= dessa forma";
+echo PHP_EOL;
+
 echo "Ou => \r\n <= dessa forma";
+echo PHP_EOL;
+
 echo "Mas a MELHOR FORMA DE QUEBRAR LINHAS é =>" . PHP_EOL ."<= dessa forma";
+echo PHP_EOL;
 
 echo "Para criar um tab precisa digitar => \t <= dessa forma";
+echo PHP_EOL;

0be1e9690cc1241885fdbb518b07eca71c113d24
Anna Luiza <luizafistarol@gmail.com>
2023-02-28 09:05:17 -0300

    feat: add loop for

---
+++ php_1/repeticaoFor.php
@@ -0,0 +1,10 @@
+<?php 
+
+//$contador++
+//$contador + 1
+//$contador += 1
+//$contador = $contador  + 1
+
+for ($contador = 1; $contador <= 15; $contador++) {
+    echo "#$contador" . PHP_EOL;
+};

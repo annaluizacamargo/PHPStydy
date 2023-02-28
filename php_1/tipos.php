2f3df56f2ed7689323ae3a0f2d70ffeb278e7e02
Anna Luiza <luizafistarol@gmail.com>
2023-02-27 22:04:00 -0300

    feat: add tipos principais em php

---
+++ php_1/tipos.php
@@ -0,0 +1,11 @@
+<?php
+//PHP É DINAMICAMENTE TIPADO
+
+// NÚMEROS INTEIROS = integer
+// NÚMEROS DECIMAIS = double
+// STRING = string
+// TRUE / FALSE = boolean
+
+$idade = true;
+
+echo gettype($idade);
